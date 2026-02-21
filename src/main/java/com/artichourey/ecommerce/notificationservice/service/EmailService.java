268f4e4d2481a67cd06f3e05889c5dfa2fb46fdb
Arti <arti.chourey21@gmail.com>
2026-02-21 22:22:50 +0530

    feat: add email and sms service logic

---
+++ src/main/java/com/artichourey/ecommerce/notificationservice/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.artichourey.ecommerce.notificationservice.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+	private final JavaMailSender javaMailSender;
+	
+	
+	
+	public void sendMail(String to, String subject,String body) {
+		SimpleMailMessage message= new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(body);
+		javaMailSender.send(message);
+	}
+	
+	
+}
