690fe350dfa81b551258182e266fef51959b25bf
Arti <arti.chourey21@gmail.com>
2026-03-30 22:30:00 +0530

    feat(dto): add UserResponseDto for handling user service responses

---
+++ src/main/java/com/artichourey/ecommerce/notificationservice/dto/UserResponseDto.java
@@ -0,0 +1,11 @@
+package com.artichourey.ecommerce.notificationservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+	private String id;
+    private String email;
+    private String phone;
+
+}
