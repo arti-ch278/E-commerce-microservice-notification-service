eeeb0c7092c16779c033536e2bc6981fd912fc67
Arti <arti.chourey21@gmail.com>
2026-03-30 22:26:49 +0530

    refactor(dto): update NotificationEvent structure for better data handling

--- src/main/java/com/artichourey/ecommerce/notificationservice/dto/NotificationEvent.java
+++ src/main/java/com/artichourey/ecommerce/notificationservice/dto/NotificationEvent.java
@@ -5,12 +5,11 @@ import lombok.Data;
 @Data
 public class NotificationEvent {
 	
-	private String email;
-	private String subject;
-	private String message;
-	private String type;
-	private String mobile;
-	
-	
+	private String userId;    
+    private String email;    
+    private String mobile;   
+    private String type;      // "EMAIL" or "SMS"
+    private String subject;
+    private String message;
 
 }
