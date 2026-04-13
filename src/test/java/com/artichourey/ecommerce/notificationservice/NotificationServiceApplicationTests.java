acd426248935f3035be838ee2f33b29e93d94963
Arti <arti.chourey21@gmail.com>
2026-04-13 22:45:19 +0530

    test: disable test cases

---
+++ src/test/java/com/artichourey/ecommerce/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.artichourey.ecommerce.notificationservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@Disabled
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	
+
+}
